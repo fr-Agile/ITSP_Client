1f51dc961ba9323fe7b2379e2b9b4e0deadad453
tomoya <windranshiin@gmail.com>
2013-06-02 12:51:11 +0900

    test

---
+++ ITSP_Client/src/jp/ac/titech/itpro/sds/fragile/utils/CommonUtils.java
@@ -0,0 +1,7 @@
+package jp.ac.titech.itpro.sds.fragile.utils;
+
+
+public class CommonUtils {
+	public static final String BASE_URL = "https://titechsimchat.appspot.com/";		
+	public static final String Login_URL = BASE_URL + "login";
+}
