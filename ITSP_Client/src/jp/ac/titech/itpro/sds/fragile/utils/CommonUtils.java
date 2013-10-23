054b76021a68795c1e04c36f61a1352bef8d6f0b
tomoya <windranshiin@gmail.com>
2013-10-23 11:19:42 +0900

    hogehoge

--- ITSP_Client/src/jp/ac/titech/itpro/sds/fragile/utils/CommonUtils.java
+++ ITSP_Client/src/jp/ac/titech/itpro/sds/fragile/utils/CommonUtils.java
@@ -2,6 +2,8 @@ package jp.ac.titech.itpro.sds.fragile.utils;
 
 
 public class CommonUtils {
-	public static final String BASE_URL = "http://192.168.0.8:8888/";		
+	public static final String BASE_URL = "http://192.168.0.5:8888/";		
 	public static final String Login_URL = BASE_URL + "login";
+	public static final String GCM_SENDER_ID = "345439642276";
+
 }
